09b7b8355a170c6cd283046fdadbbe1f121c7ba8
Leonardo Avendaño <leonardo.avendano@phinxlab.com>
2021-11-17 10:22:18 -0300

    commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { MyMap } from './MyMap';
+import { User } from './User';
+import { Company } from './Company';
+
+const user = new User();
+const myMap = new MyMap('map');
+
+//myMap.addUserMarker(user);
